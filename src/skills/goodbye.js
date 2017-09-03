dcc013edb589da2a0725c9dbca1732117c8038d9
Pedro Rocha <pedrogomesrocha@gmail.com>
2017-09-03 01:34:13 -0300

    Organizando arquivos para sabermos o que é teste, o que é válido, e ser mais legível o que cada coisa faz

--- src/skills/goodbye.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (controller, watsonMiddleware) {
-
-    controller.hears(['goodbye'], ['message_received'], watsonMiddleware.hear, function (bot, message) {
-        bot.reply(message, message.watsonData.output.text[0]);
-    });
-
-}
--- src/skills/test_viewSessionDetails.js
+++ src/skills/test_viewSessionDetails.js
@@ -19,20 +19,4 @@ module.exports = function (controller) {
         });
     });
 
-    controller.hears(['presenter_details_'+'(.*)'],['message_received'], function (bot, message) {
-        var presenterID = message.match[1];
-        console.log('PRESENTER ID',presenterID);
-
-        bot.startTyping(message, function () {
-            PresenterService().getDetails(presenterID)
-            .then(function (items) {
-                BotUI().showPresenterDetails(bot, message, items)
-            })
-            .catch(function (err) {
-                console.log('ERROR SessionService().getDetails()');
-                console.log(err);
-            });
-        });
-    });
-
-}
+};
