3e74684a13f93e842cf85f9034c1b1d8cd9ca00c
Pedro Rocha <pedrogomesrocha@gmail.com>
2017-09-05 12:55:41 -0300

    Atualizando o mapa para usar imagem da CDN do Facebook

--- src/skills/menuMap.js
+++ src/skills/menuMap.js
@@ -5,7 +5,7 @@ module.exports = function (controller, watsonMiddleware) {
   controller.hears(['PAYLOAD_MAP', 'Mapa', 'Mapa do Evento'], ['message_received'], function (bot, message) {
 
     bot.reply(message, 'Se perde não, hein! ;)');
-    url = 'https://hacktown-bot-2017-dev.herokuapp.com/hacktown_mapa.jpg';
+    url = 'https://scontent.fsdu6-1.fna.fbcdn.net/v/t31.0-8/21200863_1947435318804214_5858238536783831471_o.jpg?oh=02dc98f70cf477271eeee3626180aed7&oe=5A199B00';
     bot.reply(message, FacebookUI.imageMessage(url));
 
   });
