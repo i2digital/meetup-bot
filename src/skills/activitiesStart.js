c100fb1070e8f43b603bdb462becfea129733a26
Pedro Rocha <pedrogomesrocha@gmail.com>
2017-09-05 12:00:24 -0300

    Ajuste final do MVP

--- src/skills/activitiesStart.js
+++ src/skills/activitiesStart.js
@@ -2,7 +2,7 @@ var FacebookUI = require('../UI/FacebookUI');
 
 module.exports = function (controller, watsonMiddleware) {
 
-  controller.hears(['Programação'], ['message_received'], function (bot, message) {
+  controller.hears(['PAYLOAD_EVENT', 'Programação'], ['message_received'], function (bot, message) {
 
     button = FacebookUI.webview_button('Clique abaixo para ver a programação completa no site.', 'http://hacktown.com.br/programacao-oficial/', 'abrir site');
     bot.reply(message, button.message);
--- src/skills/menuMap.js
+++ src/skills/menuMap.js
@@ -2,7 +2,7 @@ var FacebookUI = require('../UI/FacebookUI');
 
 module.exports = function (controller, watsonMiddleware) {
 
-  controller.hears(['Mapa', 'Mapa do Evento'], ['message_received'], function (bot, message) {
+  controller.hears(['PAYLOAD_MAP', 'Mapa', 'Mapa do Evento'], ['message_received'], function (bot, message) {
 
     bot.reply(message, 'Se perde não, hein! ;)');
     url = 'https://hacktown-bot-2017-dev.herokuapp.com/hacktown_mapa.jpg';
