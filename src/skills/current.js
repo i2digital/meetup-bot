ea300eef580fa11a79b62d43dda65e75ab90eaf9
Pedro Rocha <pedrogomesrocha@gmail.com>
2017-09-08 17:15:34 -0300

    Adicionando comando 'agora'

---
+++ src/skills/current.js
@@ -0,0 +1,25 @@
+module.exports = function (controller) {
+
+  controller.hears(['agora', 'now', 'acontecendo'], ['message_received'], function (bot, message) {
+
+    BotUserService.load(message).then(function (BotUser) {
+
+      bot.startTyping(message, function () {
+        bot.reply(message, 'Ok! Vou mostrar uma lista de atividades que estão acontecendo agora)', function () {
+          bot.reply(message, 'Se prepare, pois pode ser grande ;)', function () {
+
+            SessionService().getCurrent()
+              .then(function (items) {
+                BotUI().formatSessionsList(bot, message, items);
+              })
+              .catch(function (err) {
+                console.log('Error in SessionService.getSearch()');
+                console.log(err);
+              });
+          });
+
+        });
+      });
+    });
+  });
+};
